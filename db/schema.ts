bcd6d5ee60e3280dbc1eeb35cd5f8cf172fe5d60
mzkmnk <sma0729mex@gmail.com>
2025-03-12 08:00:21 +0900

    refactor: create schema file , migration file and config file

---
+++ db/schema.ts
@@ -0,0 +1,9 @@
+import { pgTable, text, serial, timestamp } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: serial("id").primaryKey(),
+  email: text("email").notNull().unique(),
+  password: text("password").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+});
