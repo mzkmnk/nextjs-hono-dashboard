e0cf2d6cce32cde5c67acee5c41577a6e4c1c86f
mzkmnk <sma0729mex@gmail.com>
2025-03-12 21:14:16 +0900

    feat: create db connect

--- app/api/[...route]/route.ts
+++ app/api/[...route]/route.ts
@@ -1,17 +1,16 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
-import {db} from "@/db";
-import {users} from "@/db/schema";
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import { db } from "@/db/index";
+import { users } from "@/db/schema";
+export const runtime = "nodejs";
 
-export const runtime = 'nodejs'
+const app = new Hono().basePath("/api");
 
-const app = new Hono().basePath('/api')
-
-app.get('/hello', async (c) => {
+app.get("/hello", async (c) => {
   const res = await db.select().from(users);
   return c.json({
     message: res,
-  })
-})
+  });
+});
 
-export const GET = handle(app)
+export const GET = handle(app);
