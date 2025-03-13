88bd5fe3422298b27b346ac92b2305f27d4ea2f5
mzkmnk <sma0729mex@gmail.com>
2025-03-14 08:15:43 +0900

    refactor: add shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center h-screen gap-3">
+      <h1 className="text-4xl font-bold">Welcome to Hono!</h1>
       <p className="text-xl">Hello, world!</p>
+      <Button>Button</Button>
+
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
     </div>
   )
 }
