34c845adcb2d913e3d3ac1318ca1895f816da5ae
mizuki <114119396+mzkmnk@users.noreply.github.com>
2025-03-14 22:07:09 +0900

    feat(frontend): sign-in (#3)
    * feat: tailwind packageの追加
    
    * refactor: add shadcn/ui
    
    * refactor: add avatars
    
    * feat: create auth logic (wip)
    
    * refactor: rm `console.log`

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,63 @@
-'use client'
-import { useEffect, useState } from 'react'
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { Loader2 } from "lucide-react"
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const [message, setMessage] = useState()
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch('/api/hello')
-      const { message } = await res.json()
-      setMessage(message)
-    }
-    fetchData()
-  }, [])
+  const [email, setEmail] = useState<string>();
 
-  if (!message) return <p>Loading...</p>
+  const [password, setPassword] = useState<string>();
 
-  return <p>{message}</p>
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const router = useRouter();
+
+  const signIn = async () => {
+    setLoading(true);
+    await fetch('/api/auth/sign-in', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+    })
+    setLoading(false);
+
+    router.push('/internal/dashboard');
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-3">
+      <h1 className="text-4xl font-bold">Welcome to Dashboard!</h1>
+      <p>This dashboard was built using Hono.js, Next.js, Drizzle ORM, and Supabase.</p>
+
+
+      <div className="flex -space-x-4">
+        <Avatar className="border-white border-2 w-12 h-12">
+          <AvatarImage src="avatars/avatar1.png" alt='avatar' />
+          <AvatarFallback>MS</AvatarFallback>
+        </Avatar>
+        <Avatar className="border-white border-2 w-12 h-12">
+          <AvatarImage src="avatars/avatar2.png" alt='avatar' />
+          <AvatarFallback>MS</AvatarFallback>
+        </Avatar>
+      </div>
+
+      <Input placeholder="Email" className="w-1/4" onChange={(e) => setEmail(e.target.value)} />
+
+      <Input placeholder="Password" type="password" className="w-1/4" onChange={(e) => setPassword(e.target.value)} />
+
+      <Button onClick={signIn} disabled={loading}>
+        {loading ?
+          <>
+            <Loader2 className="animate-spin" />
+            Please wait...
+          </>
+          : 'Sign In'}
+      </Button>
+
+    </div >
+  )
 }
